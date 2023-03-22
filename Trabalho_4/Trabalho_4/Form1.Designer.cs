74a6d6d62cf37a45d3c679df4b6deaa4a491cc8d
Mrsbezerra <124318895+Mrsbezerra@users.noreply.github.com>
2023-03-22 20:51:48 -0300

    Add files via upload

---
+++ Trabalho_4/Trabalho_4/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Trabalho_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblDataNsc = new System.Windows.Forms.Label();
+            this.txtDataNascimento = new System.Windows.Forms.TextBox();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.lblEscola = new System.Windows.Forms.Label();
+            this.txtEscola = new System.Windows.Forms.TextBox();
+            this.lblSerie = new System.Windows.Forms.Label();
+            this.txtSerie = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(101, 226);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(101, 50);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(346, 226);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(119, 50);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(47, 70);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(486, 20);
+            this.txtNome.TabIndex = 2;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(12, 77);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 3;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblDataNsc
+            // 
+            this.lblDataNsc.AutoSize = true;
+            this.lblDataNsc.Location = new System.Drawing.Point(12, 104);
+            this.lblDataNsc.Name = "lblDataNsc";
+            this.lblDataNsc.Size = new System.Drawing.Size(92, 13);
+            this.lblDataNsc.TabIndex = 4;
+            this.lblDataNsc.Text = "Data Nascimento:";
+            // 
+            // txtDataNascimento
+            // 
+            this.txtDataNascimento.Location = new System.Drawing.Point(101, 101);
+            this.txtDataNascimento.Name = "txtDataNascimento";
+            this.txtDataNascimento.Size = new System.Drawing.Size(187, 20);
+            this.txtDataNascimento.TabIndex = 5;
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Location = new System.Drawing.Point(12, 136);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(56, 13);
+            this.lblEndereco.TabIndex = 6;
+            this.lblEndereco.Text = "Endereço:";
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(65, 129);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(468, 20);
+            this.txtEndereco.TabIndex = 7;
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(294, 104);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(52, 13);
+            this.lblTelefone.TabIndex = 8;
+            this.lblTelefone.Text = "Telefone:";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(346, 101);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(187, 20);
+            this.txtTelefone.TabIndex = 9;
+            // 
+            // lblEscola
+            // 
+            this.lblEscola.AutoSize = true;
+            this.lblEscola.Location = new System.Drawing.Point(12, 169);
+            this.lblEscola.Name = "lblEscola";
+            this.lblEscola.Size = new System.Drawing.Size(42, 13);
+            this.lblEscola.TabIndex = 10;
+            this.lblEscola.Text = "Escola:";
+            // 
+            // txtEscola
+            // 
+            this.txtEscola.Location = new System.Drawing.Point(60, 165);
+            this.txtEscola.Name = "txtEscola";
+            this.txtEscola.Size = new System.Drawing.Size(223, 20);
+            this.txtEscola.TabIndex = 11;
+            // 
+            // lblSerie
+            // 
+            this.lblSerie.AutoSize = true;
+            this.lblSerie.Location = new System.Drawing.Point(294, 169);
+            this.lblSerie.Name = "lblSerie";
+            this.lblSerie.Size = new System.Drawing.Size(34, 13);
+            this.lblSerie.TabIndex = 12;
+            this.lblSerie.Text = "Serie:";
+            // 
+            // txtSerie
+            // 
+            this.txtSerie.Location = new System.Drawing.Point(346, 166);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(187, 20);
+            this.txtSerie.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSerie);
+            this.Controls.Add(this.lblSerie);
+            this.Controls.Add(this.txtEscola);
+            this.Controls.Add(this.lblEscola);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.txtDataNascimento);
+            this.Controls.Add(this.lblDataNsc);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnOk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblDataNsc;
+        private System.Windows.Forms.TextBox txtDataNascimento;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label lblEscola;
+        private System.Windows.Forms.TextBox txtEscola;
+        private System.Windows.Forms.Label lblSerie;
+        private System.Windows.Forms.TextBox txtSerie;
+    }
+}
+
---
+++ Trabalho_4/Trabalho_4/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalho_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            
+            if (txtNome.Text == "" || txtDataNascimento.Text == "" || txtEndereco.Text == "" || txtTelefone.Text == "" || txtEscola.Text == "" || txtSerie.Text == "")
+            {
+                MessageBox.Show("Por favor, preencha todos os campos!");
+                return;
+            }
+
+            
+            DateTime dataNascimento;
+            if (DateTime.TryParse(txtDataNascimento.Text, out dataNascimento))
+            {
+                MessageBox.Show("Por favor, insira uma data de nascimento válida!");
+                return;
+            }
+
+            
+            long telefone;
+            if (!long.TryParse(txtTelefone.Text, out telefone))
+            {
+                MessageBox.Show("Por favor, insira um número de telefone válido!");
+                return;
+            }
+
+            
+            string mensagem = "Nome: " + txtNome.Text + "\nData de nascimento: " + dataNascimento.ToShortDateString() + "\nEndereço: " + txtEndereco.Text + "\nTelefone: " + telefone + "\nEscola: " + txtEscola.Text + "\nSérie: " + txtSerie.Text;
+            MessageBox.Show(mensagem);
+
+            
+            txtNome.Text = "";
+            txtDataNascimento.Text = "";
+            txtEndereco.Text = "";
+            txtTelefone.Text = "";
+            txtEscola.Text = "";
+            txtSerie.Text = "";
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            
+            txtNome.Text = "";
+            txtDataNascimento.Text = "";
+            txtEndereco.Text = "";
+            txtTelefone.Text = "";
+            txtEscola.Text = "";
+            txtSerie.Text = "";
+        }
+    }
+    
+}
